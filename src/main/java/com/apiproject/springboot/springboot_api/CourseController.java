bb6c132b629639330537daca1ead0b2cf84a134a
seannxh <nohsean6@gmail.com>
2025-04-23 16:28:19 -0500

    init

---
+++ src/main/java/com/apiproject/springboot/springboot_api/CourseController.java
@@ -0,0 +1,12 @@
+package com.apiproject.springboot.springboot_api;
+
+
+import org.springframework.web.bind.annotation.RestController;
+
+
+//Create Methods Here to make Rest Api
+
+@RestController
+public class CourseController {
+
+}
---
+++ src/test/java/com/apiproject/springboot/springboot_api/SpringbootApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apiproject.springboot.springboot_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
